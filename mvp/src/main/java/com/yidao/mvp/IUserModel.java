9d66f369106f10f5ff42b665dc6d69e4e990ab6c
xuhuanli <xuhuanli2017@gmail.com>
2018-09-08 14:25:42 +0800

    性能优化:替换view为投影

--- mvp/src/main/java/com/yidao/mvp/IUserModel.java
+++
@@ -1,8 +0,0 @@
-package com.yidao.mvp;
-
-public interface IUserModel {
-    void setID (int id);
-    void setFirstName (String firstName);
-    void setLastName (String lastName);
-    UserBean load (int id);//通过id读取user信息,返回一个UserBean
-}
--- mvp/src/main/java/com/yidao/mvp/IUserPresenter.java
+++
@@ -1,6 +0,0 @@
-package com.yidao.mvp;
-
-public interface IUserPresenter {
-    void saveUser(int id,String firstName,String lastName);
-    void loadUser(int id);
-}
