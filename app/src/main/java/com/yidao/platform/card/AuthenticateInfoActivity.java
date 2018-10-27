1a2098b3b4265b26ee980ef935b03c4721858af7
huyimin <hym4wy@163.com>
2018-10-27 11:15:05 +0800

    加入自定义相机UI和名片认证页面

---
+++ app/src/main/java/com/yidao/platform/card/AuthenticateInfoActivity.java
@@ -0,0 +1,76 @@
+package com.yidao.platform.card;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.yidao.platform.R;
+import com.yidao.platform.app.base.BaseActivity;
+
+import butterknife.ButterKnife;
+
+/**
+ * @author huyimin
+ * 身份 信息页
+ */
+
+public class AuthenticateInfoActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        TextView title = toolbar.findViewById(R.id.tb_title);
+        title.setText("请填写您的认证信息");
+        toolbar.setNavigationOnClickListener(v -> finish());
+        toolbar.setElevation(0);
+    }
+
+    public static void startAuthenticateInfoActivity(Context context) {
+        Intent intent = new Intent(context, AuthenticateInfoActivity.class);
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_authenticateinfo;
+    }
+
+    public void takePick(View view) {
+    }
+
+    /**
+     * 跳转到拍照页面
+     *
+     * @param view
+     */
+    public void toAuthenticate(View view) {
+        CameraActivity.startCameraActivity(this);
+    }
+}
